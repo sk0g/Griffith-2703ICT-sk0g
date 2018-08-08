78856ddf30d2b2f1722d813c3a09d47593c678b2
sk0g <ani.mehta@mail.com>
2018-08-09 01:39:19 +1000

    Completed laravel implementation of greeting example

---
+++ Week4/resources/views/display_greeting.blade.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+	<title>Greeting</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+        Hello {{$name}}. Next year. you will be {{$age + 1}} years old.    
+		<hr>
+</body>
+
+</html>
--- Week4/routes/web.php
+++ Week4/routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function() {
+    return "Hello!";
+});
+
+Route::get('user/{name}', function($name) {
+    return "Hello, $name.";
+});
+
+Route::get('greet', function() {
+    return view('greet');
+});
+
+Route::post('greeting', function() {
+    $name = request('name');
+    $age  = request('age');
+    $age += 1;
+
+    return view('display_greeting')
+        ->withName($name)
+        ->withAge($age);
+});
