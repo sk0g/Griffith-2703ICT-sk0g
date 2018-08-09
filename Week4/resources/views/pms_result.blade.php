aa553ec55349f68f5d0d9cf3b39b798e9ccec531
sk0g <ani.mehta@mail.com>
2018-08-09 11:38:06 +1000

    Final task for week 4 completed

---
+++ Week4/resources/views/pms_result.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts/master')
+@section('title')
+    PM Search Result
+@endsection
+
+@section('content')
+    <h2>Australian Prime Ministers</h2>
+    <h3>Results</h3>
+
+    Query: 
+    @if (!empty($name))
+        {{$name}}
+    @endif
+
+    @if (!empty($year))
+        {{$year}}
+    @endif
+
+    @if (!empty($state))
+        {{$state}}
+    @endif
+    <br>
+
+    @if (!empty($pms))
+        <table class="bordered">
+            <thead>
+            <tr><th>No.</th><th>Name</th><th>From</th><th>To</th><th>Duration</th><th>Party</th><th>State</th></tr>
+            </thead>
+        <tbody>
+
+        @foreach ($pms as $pm)
+            <tr><td>{{$pm['index']}}</td>
+                <td>{{$pm['name']}}</td>
+                <td>{{$pm['from']}}</td>
+                <td>{{$pm['to']}}</td>
+                <td>{{$pm['duration']}}</td>
+                <td>{{$pm['party']}}</td>
+                <td>{{$pm['state']}}</td></tr>
+        @endforeach
+
+    @endif
+
+    @if (!empty($pms))
+    </tbody>
+    </table>
+    @endif
+
+@endsection
--- Week4/routes/web.php
+++ Week4/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+require(app_path().'/pms.php');
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -39,3 +41,69 @@ Route::post('greeting', function() {
 Route::get('foreach', function() {
     return view('foreach')->withGet(request()->all());
 });
+
+Route::get('pms', function() {
+    return view('pms_search');
+});
+
+// To do: Perform search and display results
+Route::post('search', function()
+{
+    // Fetch form data
+    $name  = request()->input('name');
+    $year  = request()->input('year');
+    $state = request()->input('state');
+
+    // call search() to perform search
+    $pms = search($name, $year, $state);
+
+    // call view to display search result
+    return view('pms_result')
+        ->withPms($pms)
+        ->withName($name)
+        ->withYear($year)
+        ->withState($state);
+});
+
+
+/* Search sample data for $name or $year or $state from form. */
+function search($name, $year, $state)
+{
+    $pms = getPms();
+
+    // Filter $pms by $name
+    if (!empty($name)) {
+        $results = array();
+        foreach ($pms as $pm) {
+            if (stripos($pm['name'], $name) !== false) {
+                $results[] = $pm;
+            }
+        }
+        $pms = $results;
+    }
+
+    // Filter $pms by $year
+    if (!empty($year)) {
+        $results = array();
+        foreach ($pms as $pm) {
+            if (strpos($pm['from'], $year) !== false ||
+                strpos($pm['to'], $year) !== false) {
+                $results[] = $pm;
+            }
+        }
+        $pms = $results;
+    }
+
+    // Filter $pms by $state
+    if (!empty($state)) {
+        $results = array();
+        foreach ($pms as $pm) {
+            if (stripos($pm['state'], $state) !== false) {
+                $results[] = $pm;
+            }
+        }
+        $pms = $results;
+    }
+
+    return $pms;
+}
